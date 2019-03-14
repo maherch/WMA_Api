7815ba4511cb7dc119b177e9c166144a26fa0e39
maherch <maherchebl7@gmail.com>
2019-03-14 13:19:02 +0100

    Initial commit

---
+++ src/main/java/com/keystone/demo/WmaApiApplication.java
@@ -0,0 +1,13 @@
+package com.keystone.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmaApiApplication.class, args);
+	}
+
+}
